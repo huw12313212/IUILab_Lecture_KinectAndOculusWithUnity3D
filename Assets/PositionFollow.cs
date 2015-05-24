b9ef650b572b2dc9749596d2cf7f67690b04dbbb
HenryWang <huw12313212@gmail.com>
2015-05-24 22:01:17 +0800

    Kinect + Oculus

---
+++ Assets/PositionFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionFollow : MonoBehaviour {
+
+	public Transform FollowTarget;
+
+	// Update is called once per frame
+	void Update () {
+		if (FollowTarget != null) 
+		{
+			this.transform.position = FollowTarget.position;
+		}
+	}
+}
